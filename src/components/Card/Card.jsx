18a329340232bdfaabf8ac962a752fd755d99f78
Dhiraj Londhe <dhirajdipaklondhe4@gmail.com>
2024-12-30 16:44:40 +0530

    Completed the game

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,6 @@
 import Icon from "../Icon/Icon";
 
-function Card({player, onClickHandler}) {
+function Card({ gameEnd, player, onPlay, index }) {
     let icon = <Icon/>
     if(player === 'X') {
         icon = <Icon name='cross'/>
@@ -10,7 +10,7 @@ function Card({player, onClickHandler}) {
 
     return (
         <div className="p-[1rem] border w-[120px] h-[120px] rounded-[10%] bg-yellow-400 flex justify-center items-center"
-        onClick={onClickHandler}
+        onClick={() => !gameEnd && onPlay(index)}
         >
             {icon}
         </div>
--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -1,15 +1,48 @@
 import { useState } from "react";
 import Card from "../Card/Card";
-
+import isWinner from "../../helpers/checkWinner";
 
 function Grid({ numbersOfCards }) {
     const [board, setBoard] = useState(Array(numbersOfCards).fill(''));
     const [turn, setTurn] = useState(true);// true => O, false => X;
+    const [winner, setWinner] = useState(null);
+
+    function play(index) {
+        if(board[index] != "") return;
+        if(turn == true) {
+            board[index] = 'O';
+        } else {
+            board[index] = 'X';
+        }
+
+        const win = isWinner(board, turn ? "O" : "X");
+        if(win) {
+            setWinner(win);
+        }
+        setBoard([...board]);
+        setTurn(!turn);
+    }
+
+    function reset() {
+        setBoard(Array(numbersOfCards).fill(''));
+        setTurn(true);
+        setWinner(null);
+    }
     return (
         <div>
+            {
+                winner && (
+                    <>
+                    <h1 className="text-3xl">Winner is {winner}</h1>
+                    <button onClick={reset}
+                    className="px-4 py-2 bg-blue-500 text-lg"
+                    >Reset Game</button>
+                    </>
+                )
+            }
             <div className="text-3xl mb-5">Current Turn: {(turn) ? 'O' : 'X'}</div>
             <div className="flex flex-wrap w-[380px] gap-2">
-                {board.map((element, idx) => <Card key={idx}/>)}
+                {board.map((el, idx) => <Card gameEnd={winner ? true : false} key={idx} onPlay={play} player={el} index={idx}/>)}
             </div>
         </div>
         
